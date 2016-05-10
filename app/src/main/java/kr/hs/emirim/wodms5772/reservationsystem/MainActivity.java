8253c571e959bb27e1170c507e021bff201b93f7
Jaeeun Jung <wodms5772@naver.com>
2016-05-10 20:12:06 +0900

    #3 Seceond Event Programming

--- app/src/main/java/kr/hs/emirim/wodms5772/reservationsystem/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/wodms5772/reservationsystem/MainActivity.java
@@ -39,6 +39,22 @@ public class MainActivity extends AppCompatActivity {
         timePick.setVisibility(View.INVISIBLE);
         calView.setVisibility(View.INVISIBLE);
 
+        radioDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calView.setVisibility(View.VISIBLE);
+                timePick.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        radioTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calView.setVisibility(View.INVISIBLE);
+                timePick.setVisibility(View.VISIBLE);
+            }
+        });
+
     }
 
 
